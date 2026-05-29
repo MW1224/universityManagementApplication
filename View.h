277c4f7ae9fac75c304436ca206183fbeecbaffc
Mayra <90635161+MW1224@users.noreply.github.com>
2026-05-29 16:46:37 -0400

    Create View.h

---
+++ View.h
@@ -0,0 +1,18 @@
+// Presents a menu and takes input from the user
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class View
+{
+  public:
+    void menu(const vector<string>& menu, int& choice);
+    void getNumber(int& num);
+};
+
+#endif
